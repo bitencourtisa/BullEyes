753683ba52a9fad52a5f621edd3c53927c4d78e3
bianca butti <bianca.butti.almeida@accenture.com>
2019-07-01 15:58:12 -0300

    adicionar itens na view

---
+++ projetoBull/projetoBull/Controller/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  projetoBull
+//
+//  Created by isabella.bitencourt on 01/07/19.
+//  Copyright © 2019 isabella.bitencourt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var aleatorio: UILabel!
+    
+    @IBOutlet weak var desliza: UISlider!
+    
+    @IBOutlet weak var score: UILabel!
+    
+    @IBOutlet weak var round: UILabel!
+    
+    
+    @IBAction func info(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnRetornar(_ sender: UIButton) {
+    
+    
+    }
+    
+    
+    @IBAction func btnHit(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
